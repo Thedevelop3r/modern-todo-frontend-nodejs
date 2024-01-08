7a60baacfd1a8dd43aa48b5b935b20ba6ef9695f
Bilal Amjad <bilalarain444@gmail.com>
2024-01-09 00:01:15 +0500

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React, { useState } from "react";
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false); // const router = useRouter();
+
+  return <div>Login</div>;
+}
