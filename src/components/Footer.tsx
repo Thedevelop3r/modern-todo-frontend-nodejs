83847ca47be5b3d7f9e94155bbe60ffaa650fa8f
Bilal Amjad <bilalarain444@gmail.com>
2024-01-11 00:31:15 +0500

    add main footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import GeneralFooter from "./GeneralFooter";
+import DashboardFooter from "./DashboardFooter";
+import { usePathname } from "next/navigation";
+import Links from "@/utils/static.json";
+
+export default function Footer() {
+  const path = usePathname();
+  return <>{!path.includes("/dashboard") ? <GeneralFooter GenralNavbarLinks={Links.generalLinks} /> : <DashboardFooter DashboardnavLinks={Links.generalLinks} />}</>;
+}
