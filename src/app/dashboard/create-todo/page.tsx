060908a4d6ec359f5ab4875aa80f18f98f262e14
Bilal Amjad <bilalarain444@gmail.com>
2024-01-13 16:17:56 +0500

    remove interceptor page, add create todo page, update page styling

--- src/app/dashboard/create-todo/page.tsx
+++ src/app/dashboard/create-todo/page.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 export default function CreateTodo() {
   return (
-    <div>
-      <h1>Create Todo</h1>
+    <div className="flex flex-col w-full h-full">
+      <div className="py-4 px-2 bg-slate-200 rounded-md text-center font-bold text-2xl tracking-wider">
+        Create New Todo
+      </div>
     </div>
   );
 }
