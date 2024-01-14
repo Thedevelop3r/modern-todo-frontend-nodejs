46cd8cb71ec339e6ca351587289bd516ed7c0886
Bilal Amjad <bilalarain444@gmail.com>
2024-01-14 16:55:45 +0500

    fix Todos - Todo Type , add Todo type declaration

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,24 +5,33 @@ type User = {
   isLoggedIn: boolean;
 };
 
-type Todo = Array<{
-  _id: string;
-  title: string;
-  description: string;
-  isCompleted: boolean;
-  status: string;
-  createdAt: string;
-  updatedAt: string;
+type Todos = Array<{
+  _id?: string;
+  title?: string;
+  description?: string;
+  isCompleted?: boolean;
+  status?: string;
+  createdAt?: string;
+  updatedAt?: string;
 }>;
+
+type Todo = {
+  _id?: string;
+  title?: string;
+  description?: string;
+  isCompleted?: boolean;
+  status?: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
 
 type StoreState = {
   user: User;
-  todos: Todo;
+  todos: Todos;
   updateUser: ({ user, isLoggedIn }: { user: User; isLoggedIn: Boolean }) => void;
-  updateTodos: (todos: Todo) => void;
+  updateTodos: (todos: Todos) => void;
 };
 
 type STATUS_MAP_ = {
   [key: string]: string;
 };
-
